e29f6a45ca29d4db55a14d6fbe494e256ac3ebdf
Ryoma Marta-Sugawara <ryomams@users.noreply.github.com>
2022-04-14 16:16:38 -0700

    started working on rocket prefab

--- src/main.js
+++ src/main.js
@@ -16,6 +16,8 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
+// reserve keyboard vars (ok but why tho)
+let keyF, keyR, keyLEFT, keyRIGHT;
 
 
 
