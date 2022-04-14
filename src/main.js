17c2050c149cde49dd8dcf48a663afc3d6694405
Ryoma Marta-Sugawara <ryomams@users.noreply.github.com>
2022-04-14 16:05:44 -0700

    implemented scrolling starfield
    thank youuuu nathan

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 console.log("peepee")
 
+//config is config is ocnifingi
 let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
     parent: "gamegohere",
+    scene: [Menu, Play],
 }
 
-
+// let there be light!
 let game = new Phaser.Game(config);
+
+//Set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+
 
 
 
